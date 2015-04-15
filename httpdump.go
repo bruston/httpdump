afc006832e4ce074022b8d9acde8e290b5314735
Benjamin Ruston <benjy.ruston@gmail.com>
2015-04-15 23:23:15 +0100

    Fix defaultHandler when request method is OPTIONS

--- httpdump.go
+++ httpdump.go
@@ -29,9 +29,9 @@ func defaultHandler(h http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers",
 				"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		}
-		if r.Method == "OPTIONS" {
-			return
+			if r.Method == "OPTIONS" {
+				return
+			}
 		}
 		h.ServeHTTP(w, r)
 	})
